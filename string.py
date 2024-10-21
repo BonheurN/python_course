4652365bee374a081cc45f4a61927adf7c2951c7
Bonheur <ndamukunda098765@gmail.com>
2024-10-21 19:04:20 +0200

    Update and rename string.py to variables.py

--- string.py
+++
@@ -1,9 +0,0 @@
-# This is my first python program
-name = "Bro"
-food = "pizza"
-email = "ndamukundabonheur@gmail.com"
-
-print(name)
-print(f"Hello {name}")
-print(f"I like {food}")
-print(f"Did you send those documents to {email}")
---
+++ variables.py
@@ -0,0 +1,29 @@
+# This is my first python program
+name = "Bro"
+food = "pizza"
+email = "ndamukundabonheur@gmail.com"
+price = 10.99
+gpa = 3.5
+distance= 5.5
+is_student = False
+for_sale = True
+
+
+
+print(name)
+print(f"Hello {name}")
+print(f"I like {food}")
+print(f"Did you send those documents to {email}")
+print(f"The price of items is {price}")
+print(f"your gpa: {gpa}")
+print(f"you run {distance} km")
+
+if (is_student):
+    print("You are a student ")
+else:
+    print("you are not student")
+
+if for_sale:
+    print("This car is for sale")
+else:
+    print("This car is not for sale ")
