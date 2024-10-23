f8cb57da46128c9e41292849960e1105f8864c7a
Bonheur <ndamukunda098765@gmail.com>
2024-10-23 17:12:18 +0200

    Create area_of_rect.py

---
+++ area_of_rect.py
@@ -0,0 +1,5 @@
+print("Calculate the area of rectangle")
+length = int(input("Enter the length of triangle: "))
+width = int(input("Enter the width of the triangle: "))
+area = length * width
+print(f"The area of triangle is: {area}cm2")
